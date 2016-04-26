b31b1c3dd423b93048d28722f70540279b3a7d30
Stevo5o <stevo.joc@gmail.com>
2016-04-26 17:23:51 +0100

    :tada: fixed :fu:

--- build/min-js/shuffle.js
+++ build/min-js/shuffle.js
@@ -1,52 +1,90 @@
-$(function() {
-    gridContainer = $('#shuffleContainer');
-    
-    var sizer = gridContainer.find('.col-md-4');
-    
-    gridContainer.shuffle({
-        sizer: sizer,
-        speed: 500,
-        easing: 'ease-out'
-    });    
-    
-    // click button event
-    $('#btnAll').on('click', function(){
-        // shuffle function
-        gridContainer.shuffle('shuffle', function($el, shuffle){
-            return true;
-        });
-    });
-    
-    // click button event
-    $('#btnPhp').on('click', function(){
-        // shuffle function
-        gridContainer.shuffle('shuffle', function($el, shuffle){
-            return $el.data('group') == 'php';
-        });
-    });
-    
-        // click button event
-    $('#btnJs').on('click', function(){
-        // shuffle function
-        gridContainer.shuffle('shuffle', function($el, shuffle){
-            return $el.data('group') == 'js';
-        });
-    });
-    
-        // click button event
-    $('#btnTutor').on('click', function(){
-        // shuffle function
-        gridContainer.shuffle('shuffle', function($el, shuffle){
-           return $el.data('group') == 'tutor';
-        });
-    });
-  
-});
+var shuffleme = (function( $ ) {
+  'use strict';
+  var $grid = $('#grid'), //locate what we want to sort 
+      $filterOptions = $('.portfolio-sorting li'),  //locate the filter categories
+      $sizer = $grid.find('.shuffle_sizer'),    //sizer stores the size of the items
 
-$(function() {
-    $("#btnAll").trigger("click");
-});
+  init = function() {
 
-$('#container').imagesLoaded().always( function( instance ) {
-  console.log('ALWAYS - all images have been loaded');
+    // None of these need to be executed synchronously
+    setTimeout(function() {
+      listen();
+      setupFilters();
+    }, 100);
+
+    // instantiate the plugin
+    $grid.shuffle({
+      itemSelector: '[class*="col-"]',
+      sizer: $sizer    
+    });
+  },
+
+      
+
+  // Set up button clicks
+  setupFilters = function() {
+    var $btns = $filterOptions.children();
+    $btns.on('click', function(e) {
+      e.preventDefault();
+      var $this = $(this),
+          isActive = $this.hasClass( 'active' ),
+          group = isActive ? 'all' : $this.data('group');
+
+      // Hide current label, show current label in title
+      if ( !isActive ) {
+        $('.portfolio-sorting li a').removeClass('active');
+      }
+
+      $this.toggleClass('active');
+
+      // Filter elements
+      $grid.shuffle( 'shuffle', group );
+    });
+
+    $btns = null;
+  },
+
+  // Re layout shuffle when images load. This is only needed
+  // below 768 pixels because the .picture-item height is auto and therefore
+  // the height of the picture-item is dependent on the image
+  // I recommend using imagesloaded to determine when an image is loaded
+  // but that doesn't support IE7
+  listen = function() {
+    var debouncedLayout = $.throttle( 300, function() {
+      $grid.shuffle('update');
+    });
+
+    // Get all images inside shuffle
+    $grid.find('img').each(function() {
+      var proxyImage;
+
+      // Image already loaded
+      if ( this.complete && this.naturalWidth !== undefined ) {
+        return;
+      }
+
+      // If none of the checks above matched, simulate loading on detached element.
+      proxyImage = new Image();
+      $( proxyImage ).on('load', function() {
+        $(this).off('load');
+        debouncedLayout();
+      });
+
+      proxyImage.src = this.src;
+    });
+
+    // Because this method doesn't seem to be perfect.
+    setTimeout(function() {
+      debouncedLayout();
+    }, 500);
+  };      
+
+  return {
+    init: init
+  };
+}( jQuery ));
+
+$(document).ready(function()
+{
+  shuffleme.init(); //filter portfolio
 });
